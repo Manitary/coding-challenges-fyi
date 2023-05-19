e83d9696e1838511a62d73a60a301b2dda6efefd
Manitary <pak.ska@gmail.com>
2023-05-19 14:42:24 +0100

    wc part 1 add -c flag

--- src/wc/wc.py
+++ src/wc/wc.py
@@ -11,6 +11,8 @@ def wc(file_path: str) -> int:
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("file_path", action="store", nargs="+")
+    parser.add_argument("-c", "--bytes", action="store_true")
     args = parser.parse_args()
     for file_path in args.file_path:
-        print(wc(file_path))
+        if args.bytes:
+            print(wc(file_path))
