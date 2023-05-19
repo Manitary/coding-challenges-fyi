befc0769743234ccf4838a6c98116b8776d3148b
Manitary <pak.ska@gmail.com>
2023-05-19 15:59:33 +0100

    Add -l flag to wc

--- src/wc/__init__.py
+++ src/wc/__init__.py
@@ -1,3 +1,3 @@
-from .wc import main, wc
+from .wc import main, wc_bytes, wc_lines
 
-__all__ = ["main", "wc"]
+__all__ = ["main", "wc_bytes", "wc_lines"]
--- src/wc/wc.py
+++ src/wc/wc.py
@@ -5,15 +5,24 @@ import os
 import argparse
 
 
-def wc(file_path: str | Path) -> int:
+def wc_bytes(file_path: str | Path) -> int:
     return os.path.getsize(file_path)
+
+
+def wc_lines(file_path: str | Path) -> int:
+    with open(file_path, "rb") as f:
+        num_lines = sum(1 for _ in f)
+    return num_lines
 
 
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("file_path", action="store", nargs="+")
     parser.add_argument("-c", "--bytes", action="store_true")
+    parser.add_argument("-l", "--lines", action="store_true")
     args = parser.parse_args()
     for file_path in args.file_path:
         if args.bytes:
-            print(f"{wc(file_path)} {file_path}")
+            print(f"{wc_bytes(file_path)} {file_path}")
+        if args.lines:
+            print(f"{wc_lines(file_path)} {file_path}")
