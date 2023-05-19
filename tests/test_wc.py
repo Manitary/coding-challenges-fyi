7e3b8ffa76d3b8adceef76e977ef082528ca8402
Manitary <pak.ska@gmail.com>
2023-05-19 14:39:08 +0100

    Add wc part 1cli test

--- tests/test_wc.py
+++ tests/test_wc.py
@@ -1,7 +1,14 @@
 """Tests for wc."""
 
+import os
 from pathlib import Path
 import wc
+
+
+def test_entrypoint() -> None:
+    """Test that the command runs."""
+    exit_status = os.system("ccwc --help")
+    assert exit_status == 0
 
 
 def test_wc_bytes(sample_txt: Path) -> None:
@@ -9,3 +16,11 @@ def test_wc_bytes(sample_txt: Path) -> None:
     expected = 341836
     result = wc.wc(sample_txt)
     assert expected == result
+
+
+def test_wc_bytes_cli(capfd, sample_txt: Path) -> None:
+    """Test wc -c."""
+    exit_status = os.system(f"ccwc -c {sample_txt}")
+    captured = capfd.readouterr()
+    assert exit_status == 0
+    assert captured.out.strip() == "341836"
