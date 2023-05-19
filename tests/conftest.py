ae2481c06d90278be4a89e644a2884ee0fd7e2ff
Manitary <pak.ska@gmail.com>
2023-05-19 15:59:07 +0100

    Refactor test, add wc part 2 test
    The fixture holds more data which can be pulled during tests.
    The data is all in conftest so it is easier to change/update.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,7 @@
 """Test fixtures."""
 
 import shutil
+from dataclasses import dataclass
 from pathlib import Path
 from typing import Generator
 import pytest
@@ -8,10 +9,20 @@ import pytest
 ASSET_ROOT = Path(__file__).parent.resolve() / "assets"
 
 
+@dataclass
+class WCFixture:
+    """Hold data used for testing wc."""
+
+    name: str
+    path: Path
+    bytes: int
+    lines: int
+
+
 @pytest.fixture
-def sample_txt(
-    tmp_path: Path, file_name: str = "test.txt"
-) -> Generator[Path, None, None]:
-    """Yield the path to a test txt file."""
+def sample_txt(tmp_path: Path) -> Generator[WCFixture, None, None]:
+    """Yield a text file data used to test wc."""
+    file_name = "test.txt"
     copy_path = shutil.copy2(ASSET_ROOT / file_name, tmp_path / file_name)
-    yield copy_path
+    fixture_data = WCFixture(name=file_name, path=copy_path, bytes=341836, lines=7137)
+    yield fixture_data
--- tests/test_wc.py
+++ tests/test_wc.py
@@ -1,8 +1,8 @@
 """Tests for wc."""
 
 import os
-from pathlib import Path
 import pytest
+from conftest import WCFixture
 import wc
 
 
@@ -12,16 +12,29 @@ def test_entrypoint() -> None:
     assert exit_status == 0
 
 
-def test_wc_bytes(sample_txt: Path) -> None:
+def test_wc_bytes(sample_txt: WCFixture) -> None:
     """Test that wc returns number of bytes in a file."""
-    expected = 341836
-    result = wc.wc(sample_txt)
-    assert expected == result
+    result = wc.wc_bytes(sample_txt.path)
+    assert result == sample_txt.bytes
 
 
-def test_wc_bytes_cli(capfd: pytest.CaptureFixture[str], sample_txt: Path) -> None:
+def test_wc_bytes_cli(capfd: pytest.CaptureFixture[str], sample_txt: WCFixture) -> None:
     """Test wc -c."""
-    exit_status = os.system(f"ccwc -c {sample_txt}")
+    exit_status = os.system(f"ccwc -c {sample_txt.path}")
     captured = capfd.readouterr()
     assert exit_status == 0
-    assert captured.out.strip() == f"341836 {sample_txt}"
+    assert captured.out.rstrip() == f"{sample_txt.bytes} {sample_txt.path}"
+
+
+def test_wc_lines(sample_txt: WCFixture) -> None:
+    """Test that wc returns the number of lines in a file."""
+    result = wc.wc_lines(sample_txt.path)
+    assert result == sample_txt.lines
+
+
+def test_wc_lines_cli(capfd: pytest.CaptureFixture[str], sample_txt: WCFixture) -> None:
+    """Test wc -l."""
+    exit_status = os.system(f"ccwc -l {sample_txt.path}")
+    captured = capfd.readouterr()
+    assert exit_status == 0
+    assert captured.out.rstrip() == f"{sample_txt.lines} {sample_txt.path}"
