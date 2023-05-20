d40e9e5fcd681928631fbd34bfada2678d62ad36
Manitary <pak.ska@gmail.com>
2023-05-20 15:05:22 +0100

    Update json test

--- tests/test_json.py
+++ tests/test_json.py
@@ -7,5 +7,14 @@ import json_parser
 def test_json_parser(sample_json: JSONTestFile) -> None:
     """Test the JSON files."""
     # result = json_parser.parse(sample_json.path)
-    result = json_parser.parse(sample_json)
-    assert sample_json.result == result
+    with open(sample_json.path, encoding="utf8") as f:
+        lexer = json_parser.Lexer(f.read())
+    try:
+        lexer.tokenize()
+        for token in lexer.tokens:
+            print(f"{token.type} | {token.value}")
+        parser = json_parser.JSONParser(lexer.tokens)
+        result = parser.validate()
+        assert sample_json.result == result
+    except json_parser.JSONLexerError:
+        assert sample_json.result is False
