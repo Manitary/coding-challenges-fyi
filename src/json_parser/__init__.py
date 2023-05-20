675f0aeffc6e9b62b46a529cdf0772d26d692635
Manitary <pak.ska@gmail.com>
2023-05-20 15:10:53 +0100

    Add JSON validator

--- src/json_parser/__init__.py
+++ src/json_parser/__init__.py
@@ -1,5 +1,5 @@
 """A JSON Parser."""
 
-from .json import Lexer, JSONParser
+from .json import Lexer, JSONParser, JSONLexerError
 
-__all__ = ["Lexer", "JSONParser"]
+__all__ = ["Lexer", "JSONParser", "JSONLexerError"]
--- src/json_parser/json.py
+++ src/json_parser/json.py
@@ -31,11 +31,19 @@ TOKEN_DEFINITION: list[tuple[TokenType, re.Pattern[str]]] = sorted(
         (TokenType.WHITESPACE, re.compile(r"\s+")),
         (TokenType.COLON, re.compile(r":")),
         (TokenType.COMMA, re.compile(r",")),
-        (TokenType.CLOSE_BRACKET, re.compile(r"\]")),
         (TokenType.OPEN_BRACKET, re.compile(r"\[")),
+        (TokenType.CLOSE_BRACKET, re.compile(r"\]")),
         (TokenType.OPEN_BRACE, re.compile(r"{")),
         (TokenType.CLOSE_BRACE, re.compile(r"}")),
-        (TokenType.NUMBER, re.compile(r"\d+")),
+        (
+            TokenType.NUMBER,
+            re.compile(r"-?(?:0|[1-9]\d*)(?:\.\d+)?(?:[eE][+-]?\d+)?"),
+        ),
+        (
+            TokenType.STRING,
+            re.compile(r"\"(?:[^\"\\\n\t]|\\[\"\\/bfnrt]|\\u[a-fA-F0-9]{4})*\""),
+        ),
+        (TokenType.NULL, re.compile(r"null")),
     ],
 )
 
@@ -47,6 +55,10 @@ class Token:
     pos: int
     type: TokenType
     value: str
+
+
+class JSONLexerError(ValueError):
+    """Raise when the string cannot be tokenized properly."""
 
 
 class Lexer:
@@ -77,9 +89,139 @@ class Lexer:
 
         chunk = self._text[self._pos : self._pos + 20]
         msg = f"Unrecognized token: position={self._pos} content={chunk!r}"
-        raise ValueError(msg)
+        raise JSONLexerError(msg)
+
+    @property
+    def tokens(self) -> list[Token]:
+        """Return the list of tokens."""
+        return self._tokens
 
 
 class JSONParser:
     """JSON Parser."""
-    
+
+    def __init__(self, tokens: list[Token], max_nesting: int = 19) -> None:
+        self._tokens = tokens
+        self._pos = 0
+        self._nesting_level = 0
+        self._nesting_max = max_nesting
+
+    def current_type(self) -> TokenType:
+        """Return the type of the token at the current pointer."""
+        return self._tokens[self._pos].type
+
+    @property
+    def nesting_level(self) -> int:
+        """Return the current nesting level."""
+        return self._nesting_level
+
+    @property
+    def too_deep(self) -> bool:
+        """Return whether the maximum nesting level was passed."""
+        return self.nesting_level > self._nesting_max
+
+    def next(self) -> None:
+        """Advance the pointer."""
+        self._pos += 1
+
+    def depth_increase(self) -> None:
+        """Increase nesting level."""
+        self._nesting_level += 1
+
+    def depth_decrease(self) -> None:
+        """Decrease nesting level."""
+        self._nesting_level -= 1
+
+    def skip_whitespace(self) -> None:
+        """Skip whitespace token."""
+        if self.current_type() == TokenType.WHITESPACE:
+            self.next()
+
+    def validate(self) -> bool:
+        """Return whether the list of tokens matches a valid JSON payload."""
+        try:
+            if self.current_type() == TokenType.OPEN_BRACE:
+                if not self.validate_object():
+                    return False
+            elif self.current_type() == TokenType.OPEN_BRACKET:
+                if not self.validate_array():
+                    return False
+            else:
+                return False
+            self.skip_whitespace()
+            if self.current_type() != TokenType.EOF:
+                return False
+            return True
+        except IndexError:
+            return False
+
+    def validate_object(self) -> bool:
+        """Verify that the sequence of tokens is a ``object``."""
+        self.depth_increase()
+        if self.too_deep:
+            return False
+        self.next()  # Skip {
+        self.skip_whitespace()
+        while self.current_type() != TokenType.CLOSE_BRACE:
+            if self.current_type() != TokenType.STRING:
+                return False
+            self.next()
+            self.skip_whitespace()
+            if self.current_type() != TokenType.COLON:
+                return False
+            self.next()
+            if not self.validate_value():
+                return False
+            # Skip ,
+            if self.current_type() == TokenType.COMMA:
+                self.next()
+                self.skip_whitespace()
+                # , cannot be followed by }
+                if self.current_type() == TokenType.CLOSE_BRACE:
+                    return False
+        self.next()  # Skip }
+        self.depth_decrease()
+        return True
+
+    def validate_array(self) -> bool:
+        """Verify that the sequence of tokens is a ``array``."""
+        self.depth_increase()
+        if self.too_deep:
+            return False
+        self.next()  # Skip [
+        self.skip_whitespace()
+        while self.current_type() != TokenType.CLOSE_BRACKET:
+            if not self.validate_value():
+                return False
+            # self.next()
+            # Skip ,
+            if self.current_type() == TokenType.COMMA:
+                self.next()
+                # , cannot be followed by ]
+                if self.current_type() == TokenType.CLOSE_BRACKET:
+                    return False
+        self.next()  # Skip ]
+        self.depth_decrease()
+        return True
+
+    def validate_value(self) -> bool:
+        """Verify that the sequence of tokens is a ``value``."""
+        self.skip_whitespace()
+        # Test for valid values
+        if self.current_type() in {
+            TokenType.NULL,
+            TokenType.FALSE,
+            TokenType.TRUE,
+            TokenType.NUMBER,
+            TokenType.STRING,
+        }:
+            self.next()
+        elif (
+            self.current_type() == TokenType.OPEN_BRACKET and self.validate_array()
+        ) or (self.current_type() == TokenType.OPEN_BRACE and self.validate_object()):
+            # {} and [] will advance past the right delimiter upon validation
+            pass
+        else:
+            return False
+        self.skip_whitespace()
+        return True
